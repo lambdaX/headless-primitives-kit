3f66c48127660f3f2aa497a403d4bc5e7b7993ec
Nashri Online <nashri.online@gmail.com>
2025-06-15 23:09:29 +0000

    Run npm run build
    > headless-primitives-kit@0.1.0 build
    > npm run build

--- src/components/headless-logic/headless-component.ts
+++ src/components/headless-logic/headless-component.ts
@@ -263,8 +263,7 @@ export abstract class HeadlessComponent<TState extends BaseComponentState> exten
 
     /**
      * Notifies all subscribed observers of a particular event.
-     * This method is inherited from EventEmitter and re-declared here to ensure
-     * it's explicitly part of HeadlessComponent's type surface for subclasses.
+     * This explicitly defines notifyObservers on HeadlessComponent for clearer type inheritance.
      * @param event The name of the event to notify observers about.
      * @param data Optional data to pass to the event callbacks.
      */
