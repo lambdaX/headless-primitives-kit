9979f1491ce4ee0afd62a4a362ca8bb7a0f4bc34
Nashri Online <nashri.online@gmail.com>
2025-06-15 13:35:34 +0000

    allow importing each component independatly

--- src/components/headless-logic/index.ts
+++ src/components/headless-logic/index.ts
@@ -1,14 +1,42 @@
 
-export * from './event-emitter';
-export * from './command';
-export * from './component-states';
-export * from './interaction-strategies';
-export * from './headless-component';
-export * from './headless-toggle';
-export * from './headless-button';
-export * from './headless-input';
-export * from './headless-checkbox';
-export * from './headless-radio-group';
-export * from './headless-slider';
-export * from './headless-accordion';
-export * from './headless-tabs';
+export { EventEmitter, type EventCallback } from './event-emitter';
+export { Command, CommandInvoker, type CommandData } from './command';
+export {
+  ComponentState,
+  IdleState,
+  HoveredState,
+  FocusedState,
+  PressedState,
+  DisabledState,
+  LoadingState,
+  ErrorState
+} from './component-states';
+export {
+  InteractionStrategy,
+  ToggleClickStrategy,
+  ButtonClickStrategy,
+  HoverStrategy,
+  FocusStrategy,
+  KeyboardStrategy,
+  InputTextStrategy,
+  RadioItemSelectStrategy,
+  SliderUpdateStrategy,
+  SliderKeyboardStrategy,
+  AccordionToggleItemStrategy,
+  TabsActivateTabStrategy,
+  type InteractionPayload,
+  type InteractionResult
+} from './interaction-strategies';
+export {
+  HeadlessComponent,
+  type BaseComponentState,
+  type CssState
+} from './headless-component';
+export { HeadlessToggle, type ToggleState } from './headless-toggle';
+export { HeadlessButton, type ButtonState } from './headless-button';
+export { HeadlessInput, type InputState } from './headless-input';
+export { HeadlessCheckbox, type CheckboxState } from './headless-checkbox';
+export { HeadlessRadioGroup, type RadioGroupState, type RadioOption } from './headless-radio-group';
+export { HeadlessSlider, type SliderState } from './headless-slider';
+export { HeadlessAccordion, type AccordionState } from './headless-accordion';
+export { HeadlessTabs, type TabsState } from './headless-tabs';
